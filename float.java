7c4f59bdd4cfd1be77db42b82110c619b5e99236
krishnajothi <43032756+krishnajothi@users.noreply.github.com>
2018-09-07 11:42:28 +0530

    Add files via upload

---
+++ float.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int a=5;
+    float b=3,c;
+    c=a+b;
+    System.out.println("Add the two numbers:"+c);
+    c=a-b;
+    System.out.println("subract the two numbers:"+c);
+    c=a*b;
+    System.out.println("multiplication of the two numbers:"+c);
+    c=a/b;
+    System.out.println("division of the two numbers:"+c);
+  }
+}
