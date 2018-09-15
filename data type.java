2925a06fc6d89a3b1202504809b5708e0cc3a0a9
krishnajothi <43032756+krishnajothi@users.noreply.github.com>
2018-09-15 15:40:32 +0530

    Add files via upload

---
+++ data type.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    String name = "Sridevi";
+    System.out.println("Name:"+name);
+    String city = "Chennai";
+    System.out.println("City:"+city);
+    byte age = 32;
+    System.out.println("Age:"+age);
+    float height = 5;
+    System.out.println("Height:"+height);
+    boolean married = true;
+    System.out.println("Married:"+married);
+    char sex = 'F';
+    System.out.println("Sex:"+sex);
+    double citypopulation = 4.828853;
+    System.out.println("Citypopulation:"+citypopulation);	
+  }
+}
