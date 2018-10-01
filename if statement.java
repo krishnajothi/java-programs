8ff60c24d4da7959ae1ef8483bcbcdd9be1e3bcc
krishnajothi <43032756+krishnajothi@users.noreply.github.com>
2018-10-01 15:10:52 +0530

    Add files via upload

---
+++ if statement.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    int DaysInTheater = 50;
+    if (DaysInTheater > 50)
+    System.out.println("DaysInTheater more than 50");
+    else
+    System.out.println("Flop");
+  }
+}
